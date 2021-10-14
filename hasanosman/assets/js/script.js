23bc94836b707add412c23c4a3a7205f11c9c88a
hasanosman2020 <hasan79.hoa@gmail.com>
2021-10-14 16:23:15 +0100

    The menu displays the close button on tablet and desktop views and that is a bug. The list of nav items displays the 'x' button when on tablet and desktop view and the 'x' button should be displayed only on mobile view. When the menu item is clicked on mobile view the menu slides in but when the menu is closed and the menu button is pressewd a second time the menu does not appear.

--- hasanosman/assets/js/script.js
+++ hasanosman/assets/js/script.js
@@ -1,9 +1,5 @@
+/*
 const mediaQuery = window.matchMedia('(min-width: 599px)')
 if (mediaQuery.matches) {
-  document.getElementById('closebtn').style = 'display:none'
-}
-/*
-  document.querySelector('.sidenav').style.width = '100%'
-} else {
-  document.querySelector('.sidenav').style.width = '0%'
+  document.querySelector('#closebtn').style = 'visibility: none'
 }*/
