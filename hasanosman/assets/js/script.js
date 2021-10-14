2bd117b9a704c17051c558bd5da469509d1a218b
hasanosman2020 <hasan79.hoa@gmail.com>
2021-10-14 11:11:51 +0100

    add js file

---
+++ hasanosman/assets/js/script.js
@@ -0,0 +1,9 @@
+const mediaQuery = window.matchMedia('(min-width: 599px)')
+if (mediaQuery.matches) {
+  document.getElementById('closebtn').style = 'display:none'
+}
+/*
+  document.querySelector('.sidenav').style.width = '100%'
+} else {
+  document.querySelector('.sidenav').style.width = '0%'
+}*/
